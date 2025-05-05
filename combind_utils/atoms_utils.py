e951af69fb0e60721aafefb5707c4de677af1675
Marvin Li <marvinli0327@gmail.com>
2025-05-04 22:13:37 -0700

    update

--- combind_utils/atoms_utils.py
+++ combind_utils/atoms_utils.py
@@ -4,3 +4,12 @@ def get_atoms_coordinates(atom):
     z = atom.z#property["r_m_z_coord"]
     return x, y, z
 
+def get_ligand_protein_path(ligands_dict,key):
+    if ligands_dict[key][0].contains("lig.mae"):
+        ligand = ligands_dict[key][0]
+        protein = ligands_dict[key][1]
+    else:
+        ligand = ligands_dict[key][1]
+        protein = ligands_dict[key][0]
+    return ligand, protein
+
