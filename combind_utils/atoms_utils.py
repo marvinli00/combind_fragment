053a8fa7f18983afb3593e9de676430edaf99699
Marvin Li <marvinli0327@gmail.com>
2025-05-08 11:35:57 -0700

    update

--- combind_utils/atoms_utils.py
+++ combind_utils/atoms_utils.py
@@ -1,3 +1,6 @@
+from schrodinger.structure import StructureReader
+from schrodinger.structutils.rmsd import ConformerRmsd
+import numpy as np
 def get_atoms_coordinates(atom):
     x = atom.x#property["r_m_x_coord"]
     y = atom.y#property["r_m_y_coord"]
@@ -13,3 +16,47 @@ def get_ligand_protein_path(ligands_dict,key):
         protein = ligands_dict[key][0]
     return ligand, protein
 
+# This code reads predicted ligand poses from Glide docking and stores their docking scores
+# The poses are read from a MAE file containing multiple docked conformations
+# Each pose (except the reference structure) is stored in a dictionary with its docking score
+# Lower docking scores indicate better predicted binding affinity
+
+def read_poses_and_calculate_rmsd(poses_pred_path, poses_true_path, base_name="4bz6", top = 100):
+    """
+    Read predicted poses from a MAE file and calculate RMSD values against a reference structure.
+    
+    Args:
+        poses_pred_path (str): Path to the predicted poses MAE file
+        poses_true_path (str): Path to the reference structure MAE file 
+        base_name (str): Base name to identify reference structure
+        
+    Returns:
+        tuple: Dictionary of poses and their docking scores, list of RMSD values
+    """
+    poses_true = next(StructureReader(poses_true_path))
+    
+    # Dictionary to store docking poses and their scores
+    poses_pred_structure_glide = {}
+    rmsd_values = []
+    count = 0
+
+    # Read each model from the poses file
+    with StructureReader(poses_pred_path) as reader:
+        for model in reader:
+            # Skip the reference structure
+            if base_name in model.property["s_m_title"]:
+                continue
+            else:
+                count += 1
+                if count > top:
+                    break
+                # Extract the docking score for this pose
+                docking_score = model.property["r_i_docking_score"]
+                # Store the pose model and its docking score
+                poses_pred_structure_glide[model] = docking_score
+                rmsd_value = ConformerRmsd(reference_structure=poses_true,
+                                         test_structure=model, asl_expr="NOT atom.element H").calculate()
+                # Record rmsd value
+                rmsd_values.append(rmsd_value)
+    rmsd_values = np.array(rmsd_values)
+    return poses_pred_structure_glide, rmsd_values
--- dock/struct_sort.py
+++ dock/struct_sort.py
@@ -1,10 +1,17 @@
 import os
 from schrodinger.structure import StructureReader
 
-def split_complex(st, pdb_id):
-    os.system('mkdir -p structures/proteins structures/ligands')
-    lig_path = 'structures/ligands/{}_lig.mae'.format(pdb_id)
-    prot_path = 'structures/proteins/{}_prot.mae'.format(pdb_id)
+def split_complex(st, pdb_id, root=None):
+    
+    
+    if root is None:
+        os.system('mkdir -p structures/proteins structures/ligands')
+        lig_path = 'structures/ligands/{}_lig.mae'.format(pdb_id)
+        prot_path = 'structures/proteins/{}_prot.mae'.format(pdb_id)
+    else:
+        os.system(f'mkdir -p {root}/structures/proteins {root}/structures/ligands')
+        lig_path = '{}/structures/ligands/{}_lig.mae'.format(root, pdb_id)
+        prot_path = '{}/structures/proteins/{}_prot.mae'.format(root,pdb_id)
 
     if not os.path.exists(lig_path) and len([a.index for a in st.atom if a.chain == 'L']) > 0:
         lig_st = st.extract([a.index for a in st.atom if a.chain == 'L'])
@@ -16,10 +23,13 @@ def split_complex(st, pdb_id):
         prot_st.title = '{}_prot'.format(pdb_id)
         prot_st.write(prot_path)
 
-def struct_sort(structs):
+def struct_sort(structs, root=None):
     for struct in structs:
-        opt_complex = 'structures/aligned/{}/rot-{}_query.mae'.format(struct, struct)
+        if root is None:
+            opt_complex = 'structures/aligned/{}/rot-{}_query.mae'.format(struct, struct)
+        else:
+            opt_complex = '{}/structures/aligned/{}/rot-{}_query.mae'.format(root, struct, struct)
 
         if os.path.exists(opt_complex):
             comp_st = next(StructureReader(opt_complex))
-            split_complex(comp_st, struct)
+            split_complex(comp_st, struct, root)
